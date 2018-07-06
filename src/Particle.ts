deeb56a18bd418bf03d50435a637c6994f424951
olivia_github@outlook.com <Olivia>
2018-07-06 17:07:41 +0100

    Adding initial particle generation.

---
+++ src/Particle.ts
@@ -0,0 +1,11 @@
+class Particle {
+
+    private body; 
+
+    constructor(body: Matter.Body) {
+        this.body = body;
+    }
+
+}
+
+export { Particle };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as Matter from "matter-js";
+import * as Random from "random-number";
+import { Particle } from "./Particle";
 
 // module aliases
 const Engine = Matter.Engine;
@@ -19,13 +21,37 @@ const render = Render.create({
     }
 });
 
-// create two boxes and a ground
-const boxA = Bodies.rectangle(400, 200, 80, 80);
-const boxB = Bodies.rectangle(450, 50, 80, 80);
+// create the ground
 const ground = Bodies.rectangle(400, 800, 1500, 200, { isStatic: true });
 
+// create particles
+const particles = [];
+const bodies = [];
+const particleCount = 100;
+
+for (let i = 0; i < particleCount; i++) {
+    // generate a body
+    const bodyWidth = 30;
+    const bodyHeight = 30;
+    const x = Random({
+        min: 0,
+        max: document.body.clientWidth,
+        integer: true
+    });
+    const y = Random({
+        min: 0,
+        max: document.body.clientHeight,
+        integer: true
+    });
+    console.log(`Creating particle with x=${x}, y=${y}, w=${bodyWidth}, h=${bodyHeight}`);
+    const body = Bodies.rectangle(x, y, bodyWidth, bodyHeight);
+    const particle = new Particle(body);
+    particles.push(particle);
+    bodies.push(body);
+}
+
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
+World.add(engine.world, [ground, ...bodies]);
 
 // run the engine
 Engine.run(engine);
